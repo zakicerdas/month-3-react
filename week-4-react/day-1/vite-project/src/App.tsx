e2e3c25068411deb99db656dec5a78bf73db3bfe
zakicerdas <zakitoriq1@gmail.com>
2025-10-22 13:35:07 +0700

    tugas week 4 day 1: advanced styling

---
+++ week-4-react/day-1/vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "./components/login-form"
+
+function App() {
+  return (
+    <div className="ml-100 mr-100">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
